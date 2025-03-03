6f740a35568aa6213b80e5b21f42bfebe18244d1
Sarah <sarahglenn04@gmail.com>
2025-03-03 08:59:07 -0500

    all changes for BasketballGames

--- BasketballGames/BasketballGames/ContentView.swift
+++ BasketballGames/BasketballGames/ContentView.swift
@@ -7,15 +7,74 @@
 
 import SwiftUI
 
+//struct Response: Codable {
+//    var games: [Game]
+//}
+
+
+
+struct Game: Codable {
+    var id: Int
+    var team: String
+    var opponent: String
+    var date: String
+    var isHomeGame: Bool
+    var score: Score
+}
+
+struct Score: Codable {
+    var unc: Int
+    var opponent: Int
+}
+
 struct ContentView: View {
+//    @State private var games = [Game]()
+    @State private var games: [Game] = []
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            List(games, id: \.id) { game in
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("\(game.team) vs. \(game.opponent)")
+                            .font(.headline)
+                        Text("\(game.date)")
+                            .font(.subheadline)
+                            .foregroundStyle(.secondary)
+                    }
+                    Spacer()
+                    VStack(alignment: .trailing) {
+                        Text("\(game.score.unc) - \(game.score.opponent)")
+                            .font(.headline)
+                        Text("\(game.isHomeGame ? "Home" : "Away")")
+                            .font(.subheadline)
+                            .foregroundStyle(.secondary)
+                    }
+                }
+            }
+            .navigationTitle("UNC Basketball")
         }
-        .padding()
+
+        .task {
+            await loadData()
+        }
+        
+    }
+    func loadData() async {
+        guard let url = URL(string: "https://api.samuelshi.com/uncbasketball") else {
+            print("Invalid URL")
+            return
+        }
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+//            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+            if let decodedResponse = try? JSONDecoder().decode([Game].self, from: data) {
+                games = decodedResponse
+            }
+        } catch {
+            print("Invalid data")
+        }
     }
 }
 
